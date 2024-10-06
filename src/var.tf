3bcb9da9c45de690a8e06587b34c6f062acc680b
abhinav dev <abhinav.dev@gmail.com>
2024-10-06 23:14:09 +0530

    updated var.tf

--- src/var.tf
+++ src/var.tf
@@ -3,11 +3,6 @@ variable "aws_region" {
   default     = "eu-west-1"
 }
 
-variable "key_name" {
-  description = " SSH keys to connect to ec2 instance"
-  default     =  "kmaster"
-}
-
 variable "instance_type" {
   description = "instance type for ec2"
   default     =  "t2.micro"
