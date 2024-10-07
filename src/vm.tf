323d2e7b7fe7a2838c50000709906a7f5aeff093
abhinav dev <abhinav.dev@gmail.com>
2024-10-07 23:05:30 +0530

    updated credential sourcing

--- src/vm.tf
+++ src/vm.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "ap-south-1"
-  access_key = credentials('AWS_ACCESS_KEY_ID')
-  secret_key = credentials('AWS_SECRET_ACCESS_KEY')
+  access_key = credentials("AWS_ACCESS_KEY_ID")
+  secret_key = credentials("AWS_SECRET_ACCESS_KEY")
 }
 
 #Create security group with firewall rules
