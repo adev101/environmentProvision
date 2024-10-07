e2231252e0d02cfb6323a92aaccec73e8dd5ef5e
abhinav dev <39181811+adev101@users.noreply.github.com>
2024-10-07 23:52:55 +0530

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -1,3 +1,12 @@
+terraform{
+  required_provider{
+  aws = {
+    source = "hashicorp/aws"
+    version = "~> 4.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "ap-south-1"
   access_key = var.AWS_ACCESS_KEY_ID
