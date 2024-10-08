58be9dd43a0e084f450183583914d126c2d9c812
abhinav dev <39181811+adev101@users.noreply.github.com>
2024-10-08 14:12:06 +0530

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -36,7 +36,8 @@ resource "aws_security_group" "my_security_group" {
 resource "aws_instance" "myFirstInstance" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  security_groups= [var.security_group]
+  #security_groups= [var.security_group]
+  security_groups= "default"
   tags= {
     Name = var.tag_name
   }
